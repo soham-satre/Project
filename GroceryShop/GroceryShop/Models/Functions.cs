a7c767a543acbabac743edf3dc030221add6f93d
soham-satre <sohamsatre0304@gmail.com>
2024-12-16 22:06:17 +0530

    Seller login Done

--- GroceryShop/GroceryShop/Models/Functions.cs
+++ GroceryShop/GroceryShop/Models/Functions.cs
@@ -15,34 +15,48 @@ namespace GroceryShop.Models
         private SqlDataAdapter sda;
         private string ConnString;
 
-        public Functions() {
-            ConnString =@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\soham\OneDrive\Documents\GroceryShopDb.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
-            Con=new SqlConnection(ConnString);
+        public Functions()
+        {
+            ConnString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\soham\OneDrive\Documents\GroceryShopDb.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+            Con = new SqlConnection(ConnString);
             cmd = new SqlCommand();
             cmd.Connection = Con;
-
         }
-        public DataTable getData(string Query){
-        dt=new DataTable();
-            sda=new SqlDataAdapter(Query,ConnString);
+
+        public DataTable getData(string Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, ConnString);
             sda.Fill(dt);
-             return dt;
-
+            return dt;
         }
-        public int SetData(string Query){
-        int Cnt=0;
-            if(Con.State==ConnectionState.Closed){
-            Con.Open();
+
+        public int SetData(string Query)
+        {
+            int Cnt = 0;
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
             }
-        cmd.CommandText=Query;
-            Cnt=cmd.ExecuteNonQuery();
+            cmd.CommandText = Query;
+            Cnt = cmd.ExecuteNonQuery();
             Con.Close();
             return Cnt;
         }
 
-        internal DataTable getData(string Query, Dictionary<string, object> parameters)
+        public DataTable getData(string Query, Dictionary<string, object> parameters)
         {
-            throw new NotImplementedException();
+            dt = new DataTable();
+            using (SqlCommand cmd = new SqlCommand(Query, Con))
+            {
+                foreach (var param in parameters)
+                {
+                    cmd.Parameters.AddWithValue(param.Key, param.Value);
+                }
+                sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+            }
+            return dt;
         }
     }
 }
